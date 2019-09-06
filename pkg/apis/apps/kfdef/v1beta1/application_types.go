ee6849963645e52a0aef72583ed487dcb8224015
Gabriel Wen <gabrielwen@google.com>
2019-09-06 11:08:27 -0700

    v1beta1 spec

---
+++ pkg/apis/apps/kfdef/v1beta1/application_types.go
@@ -0,0 +1,154 @@
+// Copyright 2018 The Kubeflow Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1beta1
+
+import (
+	"github.com/kubeflow/kfctl/v3/config"
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+const (
+	KfConfigFile = "app.yaml"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// KfDef is the Schema for the applications API
+// +k8s:openapi-gen=true
+type KfDef struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   KfDefSpec   `json:"spec,omitempty"`
+	Status KfDefStatus `json:"status,omitempty"`
+}
+
+type KfDefSpec struct {
+	Applications []Application `json:"applications,omitempty"`
+	Plugins      []Plugin      `json:"plugins,omitempty"`
+	Secrets      []Secret      `json:"secrets,omitempty"`
+	Repos        []Repo        `json:"repos,omitempty"`
+}
+
+// Application defines an application to install
+type Application struct {
+	Name            string           `json:"name,omitempty"`
+	KustomizeConfig *KustomizeConfig `json:"kustomizeConfig,omitempty"`
+}
+
+type KustomizeConfig struct {
+	RepoRef    *RepoRef           `json:"repoRef,omitempty"`
+	Overlays   []string           `json:"overlays,omitempty"`
+	Parameters []config.NameValue `json:"parameters,omitempty"`
+}
+
+type RepoRef struct {
+	Name string `json:"name,omitempty"`
+	Path string `json:"path,omitempty"`
+}
+
+// Plugin can be used to customize the generation and deployment of Kubeflow
+type Plugin struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec *runtime.Object `json:"spec,omitempty"`
+}
+
+// Secret provides information about secrets needed to configure Kubeflow.
+// Secrets can be provided via references e.g. a URI so that they won't
+// be serialized as part of the KfDefSpec which is intended to be written into source control.
+type Secret struct {
+	Name         string        `json:"name,omitempty"`
+	SecretSource *SecretSource `json:"secretSource,omitempty"`
+}
+
+type SecretSource struct {
+	LiteralSource *LiteralSource `json:"literalSource,omitempty"`
+	HashedSource  *HashedSource  `json:"hashedSource,omitempty"`
+	EnvSource     *EnvSource     `json:"envSource,omitempty"`
+}
+
+type LiteralSource struct {
+	Value string `json:"value,omitempty"`
+}
+
+type HashedSource struct {
+	HashedValue string `json:"value,omitempty"`
+}
+
+type EnvSource struct {
+	Name string `json:"Name,omitempty"`
+}
+
+// Repo provides information about a repository providing config (e.g. kustomize packages,
+// Deployment manager configs, etc...)
+type Repo struct {
+	// Name is a name to identify the repository.
+	Name string `json:"name,omitempty"`
+	// URI where repository can be obtained.
+	// Can use any URI understood by go-getter:
+	// https://github.com/hashicorp/go-getter/blob/master/README.md#installation-and-usage
+	Uri string `json:"uri,omitempty"`
+}
+
+// KfDefStatus defines the observed state of KfDef
+type KfDefStatus struct {
+	Conditions []KfDefCondition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,6,rep,name=conditions"`
+	// ReposCache is used to cache information about local caching of the URIs.
+	ReposCache map[string]RepoCache `json:"reposCache,omitempty"`
+}
+
+type RepoCache struct {
+	LocalPath string `json:"localPath,string"`
+}
+
+type KfDefConditionType string
+
+const (
+	// KfCreated means the KfDef spec has been created.
+	KfCreated KfDefConditionType = "Created"
+
+	// KfDeploying means Kubeflow is in the process of being deployed.
+	KfDeploying KfDefConditionType = "Deploying"
+
+	// KfSucceeded means Kubeflow was successfully deployed.
+	KfSucceeded KfDefConditionType = "Succeeded"
+
+	// KfFailed meansthere was a problem deploying Kubeflow.
+	KfFailed KfDefConditionType = "Failed"
+
+	// Reasons for conditions
+
+	// InvalidKfDefSpecReason indicates the KfDef was not valid.
+	InvalidKfDefSpecReason = "InvalidKfDefSpec"
+)
+
+type KfDefCondition struct {
+	// Type of deployment condition.
+	Type KfDefConditionType `json:"type" protobuf:"bytes,1,opt,name=type,casttype=KfDefConditionType"`
+	// Status of the condition, one of True, False, Unknown.
+	Status v1.ConditionStatus `json:"status" protobuf:"bytes,2,opt,name=status,casttype=k8s.io/api/core/v1.ConditionStatus"`
+	// The last time this condition was updated.
+	LastUpdateTime metav1.Time `json:"lastUpdateTime,omitempty" protobuf:"bytes,6,opt,name=lastUpdateTime"`
+	// Last time the condition transitioned from one status to another.
+	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty" protobuf:"bytes,7,opt,name=lastTransitionTime"`
+	// The reason for the condition's last transition.
+	Reason string `json:"reason,omitempty" protobuf:"bytes,4,opt,name=reason"`
+	// A human readable message indicating details about the transition.
+	Message string `json:"message,omitempty" protobuf:"bytes,5,opt,name=message"`
+}
