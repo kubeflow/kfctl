10f6a663a0a1ae7e0f51074bf02ad85860b21dc7
Gabriel Wen <gabrielwen@google.com>
2019-09-13 17:06:15 -0700

    switch to rawextension

--- pkg/apis/apps/kfdef/v1beta1/application_types.go
+++ pkg/apis/apps/kfdef/v1beta1/application_types.go
@@ -80,7 +80,7 @@ type Plugin struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec *runtime.Object `json:"spec,omitempty"`
+	Spec *runtime.RawExtension `json:"spec,omitempty"`
 }
 
 // Secret provides information about secrets needed to configure Kubeflow.
