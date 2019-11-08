59c0f4533bce50d5e49c84ae90bf07b55cd83bd9
Gabriel Wen <gabrielwen@google.com>
2019-11-08 15:06:24 -0800

    update import path

--- testing/e2e/kfctl_go_test.py
+++ testing/e2e/kfctl_go_test.py
@@ -3,7 +3,7 @@ import os
 
 import pytest
 
-from kubeflow.kubeflow.ci import kfctl_go_test_utils as kfctl_util
+from kfctl.ci import kfctl_go_test_utils as kfctl_util
 from kubeflow.testing import util
 
 def test_build_kfctl_go(record_xml_attribute, app_path, project, use_basic_auth,
