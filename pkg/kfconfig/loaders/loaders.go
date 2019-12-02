07762fa4a027108198bdfcdd4c98d3b838d9ea21
Gabriel Wen <gabrielwen@google.com>
2019-12-02 15:02:31 -0800

    add converters

--- pkg/kfconfig/loaders/loaders.go
+++ pkg/kfconfig/loaders/loaders.go
@@ -141,6 +141,7 @@ func LoadConfigFromURI(configFile string) (*kfconfig.KfConfig, error) {
 	converters := map[string]Loader{
 		"v1alpha1": V1alpha1{},
 		"v1beta1":  V1beta1{},
+		"v1":       V1{},
 	}
 
 	converter, ok := converters[apiVersionSeparated[1]]
@@ -205,6 +206,7 @@ func WriteConfigToFile(config kfconfig.KfConfig) error {
 	converters := map[string]Loader{
 		"v1alpha1": V1alpha1{},
 		"v1beta1":  V1beta1{},
+		"v1":       V1{},
 	}
 	apiVersionSeparated := strings.Split(config.APIVersion, "/")
 	if len(apiVersionSeparated) < 2 || apiVersionSeparated[0] != Api {
