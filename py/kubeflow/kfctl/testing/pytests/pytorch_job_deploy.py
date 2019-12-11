ccd167f88f3ef6e7ea84749b4aa1ac059e2e5b19
Gabriel Wen <gabrielwen@google.com>
2019-12-11 14:37:32 -0800

    kubectl apply -f

--- py/kubeflow/kfctl/testing/pytests/pytorch_job_deploy.py
+++ py/kubeflow/kfctl/testing/pytests/pytorch_job_deploy.py
@@ -15,10 +15,11 @@ from kubernetes import client as k8s_client
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 
-def deploy_pytorchjob(record_xml_attribute):
+def deploy_pytorchjob(record_xml_attribute, kfctl_repo_path):
   """Deploy PytorchJob."""
-  util.load_kube_config(persist_config=False)
-  api_client = k8s_client.ApiClient()
+  util.load_kube_credentials()
+  util.run(["kubectl", "apply", "-f", "testdata/pytorch_job.yaml"],
+           cwd = os.path.join(kfctl_repo_path, "py/kubeflow/kfctl/testing/pytests"))
 
 if __name__ == "__main__":
   logging.basicConfig(level=logging.INFO,
