10f9a984ea5cb0f4ee4ea8a8af9b6199ed03e943
Gabriel Wen <gabrielwen@google.com>
2019-12-11 15:08:05 -0800

    update workingdir

--- py/kubeflow/kfctl/testing/ci/kfctl_e2e_workflow.py
+++ py/kubeflow/kfctl/testing/ci/kfctl_e2e_workflow.py
@@ -381,6 +381,7 @@ class Builder:
     dependences = []
     pytorch_test = self._build_step(step_name, self.workflow, TESTS_DAG_NAME, task_template,
                                     command, dependences)
+    pytorch_test["container"]["workingDir"] = self.kfctl_pytest_dir
     #***************************************************************************
     # Notebook test
 
