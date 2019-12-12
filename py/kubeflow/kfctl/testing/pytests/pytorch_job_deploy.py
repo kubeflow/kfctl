df27f579b0348d2210deeec3e7e07f5f4f16f895
Gabriel Wen <gabrielwen@google.com>
2019-12-12 13:28:32 -0800

    check if pods are spawned

--- py/kubeflow/kfctl/testing/pytests/pytorch_job_deploy.py
+++ py/kubeflow/kfctl/testing/pytests/pytorch_job_deploy.py
@@ -23,6 +23,27 @@ def test_deploy_pytorchjob(record_xml_attribute, kfctl_repo_path):
   util.run(["kubectl", "apply", "-f",
             os.path.join(kfctl_repo_path,
                          "py/kubeflow/kfctl/testing/pytests/testdata/pytorch_job.yaml")])
+  api_client = k8s_client.ApiClient()
+  api = k8s_client.CoreV1Api(api_client)
+
+  # If the call throws exception, let it emit as an error case.
+  resp = api.list_namespaced_pod("default")
+  names = {
+      "pytorch-mnist-ddp-cpu-master-0": False,
+      "pytorch-mnist-ddp-cpu-worker-0": False,
+  }
+
+  for pod in resp.items:
+    name = pod.metadata.name
+    if name in names:
+      names[name] = True
+
+  msg = []
+  for n in names:
+    if not names[n]:
+      msg.append("pod %s is not found" % n)
+  if msg:
+    raise ValueError("; ".join(msg))
 
 if __name__ == "__main__":
   logging.basicConfig(level=logging.INFO,
