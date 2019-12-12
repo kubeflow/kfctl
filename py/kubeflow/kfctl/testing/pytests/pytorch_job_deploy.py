aa2da4403b066dfd081af3d5a3a23287a27c80f5
Gabriel Wen <gabrielwen@google.com>
2019-12-11 20:34:46 -0800

    try remove cwd

--- py/kubeflow/kfctl/testing/pytests/pytorch_job_deploy.py
+++ py/kubeflow/kfctl/testing/pytests/pytorch_job_deploy.py
@@ -18,8 +18,7 @@ from oauth2client.client import GoogleCredentials
 def test_deploy_pytorchjob(record_xml_attribute, kfctl_repo_path):
   """Deploy PytorchJob."""
   util.load_kube_credentials()
-  util.run(["kubectl", "apply", "-f", "testdata/pytorch_job.yaml"],
-           cwd = os.path.join(kfctl_repo_path, "py/kubeflow/kfctl/testing/pytests"))
+  util.run(["kubectl", "apply", "-f", "testdata/pytorch_job.yaml"])
 
 if __name__ == "__main__":
   logging.basicConfig(level=logging.INFO,
