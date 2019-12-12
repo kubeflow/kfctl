8bc97f5920d63325abd310708b80b379a6af5157
Gabriel Wen <gabrielwen@google.com>
2019-12-12 11:30:31 -0800

    temp

--- py/kubeflow/kfctl/testing/pytests/pytorch_job_deploy.py
+++ py/kubeflow/kfctl/testing/pytests/pytorch_job_deploy.py
@@ -21,7 +21,7 @@ def test_deploy_pytorchjob(record_xml_attribute, kfctl_repo_path):
   util.load_kube_config()
   util.load_kube_credentials()
   logging.info("using kfctl repo: %s" % kfctl_repo_path)
-  time.sleep(3600)
+  time.sleep(1800)
   util.run(["kubectl", "apply", "-f",
             os.path.join(kfctl_repo_path,
                          "py/kubeflow/kfctl/testing/pytests/testdata/pytorch_job.yaml")])
