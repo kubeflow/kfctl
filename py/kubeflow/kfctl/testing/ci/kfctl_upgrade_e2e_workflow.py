425c9f6a53a9d1cdf09787e7e9e11616b32629d6
Richard Liu <ricliu@google.com>
2020-01-14 11:40:34 -0800

    set pytest dir

--- py/kubeflow/kfctl/testing/ci/kfctl_upgrade_e2e_workflow.py
+++ py/kubeflow/kfctl/testing/ci/kfctl_upgrade_e2e_workflow.py
@@ -90,6 +90,8 @@ class Builder(kfctl_e2e_workflow.Builder):
         # Change the workflow_template labels
         self.workflow["metadata"]["labels"]["workflow_template"] = TEMPLATE_LABEL
 
+        # Change the workfing directory for all subsequent steps
+        task_template["container"]["workingDir"] = os.path.join(self.kfctl_pytest_dir)
         # Add the dag to upgrade Kubeflow to the workflow
         self._build_upgrade_dag()
 
