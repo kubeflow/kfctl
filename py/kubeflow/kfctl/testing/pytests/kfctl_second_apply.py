c67333c63b23781a7d71a185a24fab578d2d626b
Richard Liu <ricliu@google.com>
2020-02-03 17:14:58 -0800

    remove skip

--- py/kubeflow/kfctl/testing/pytests/kfctl_second_apply.py
+++ py/kubeflow/kfctl/testing/pytests/kfctl_second_apply.py
@@ -6,8 +6,7 @@ import pytest
 from kubeflow.kfctl.testing.util import kfctl_go_test_utils as kfctl_util
 from kubeflow.testing import util
 
-@pytest.mark.skipif(os.getenv("JOB_TYPE") == "presubmit",
-                    reason="test second apply doesn't run in presubmits")
+
 def test_second_apply(record_xml_attribute, app_path, kfctl_path):
   """Test that we can run kfctl apply again with error.
 
