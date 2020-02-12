242e97c48978e5177d01e52cc330ee54cc484149
Gabriel Wen <gabrielwen@google.com>
2020-02-12 10:26:17 -0800

    img

--- py/kubeflow/kfctl/testing/ci/kfctl_e2e_workflow.py
+++ py/kubeflow/kfctl/testing/ci/kfctl_e2e_workflow.py
@@ -295,7 +295,7 @@ class Builder(object):
         {"name": "TEST_TARGET_NAME",
          "value": self.test_target_name},
        ],
-      'image': 'gcr.io/kubeflow-ci/test-worker:v20200211-f6a132a-e3b0c4',
+      'image': 'gcr.io/kubeflow-ci/test-worker:latest',
       'imagePullPolicy': 'Always',
       'name': '',
       'resources': {'limits': {'cpu': '4', 'memory': '4Gi'},
