ff104f51d8a4accbc63182a3a16f1d595108f2d4
Gabriel Wen <gabrielwen@google.com>
2020-02-17 19:09:19 -0800

    use py3 img

--- py/kubeflow/kfctl/testing/ci/kfctl_e2e_workflow.py
+++ py/kubeflow/kfctl/testing/ci/kfctl_e2e_workflow.py
@@ -295,7 +295,7 @@ class Builder(object):
         {"name": "TEST_TARGET_NAME",
          "value": self.test_target_name},
        ],
-      'image': 'gcr.io/kubeflow-ci/test-worker:latest',
+      'image': 'gcr.io/kubeflow-ci/test-worker-py3:e2e15ec',
       'imagePullPolicy': 'Always',
       'name': '',
       'resources': {'limits': {'cpu': '4', 'memory': '4Gi'},
