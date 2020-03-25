fcf01ef0837f9dbde2606d6341e657b50de2ec2e
Adrian Zhuang <wzhuang@us.ibm.com>
2020-03-25 11:48:26 -0700

    Update operator version to reflect kubeflow version (#285)
    * operator versioning
    
    * align version with kubeflow release version
    
    * keep 0.1.0
    
    * keep 0.1.0
    
    * update alm-examples link
    
    * remove 1.0.1 version

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -25,9 +25,14 @@ import (
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	log "github.com/sirupsen/logrus"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
+)
+
+// Kubeflow operator version
+var (
+	Version         string = "1.0.0"
 )
 
 // Change below variables to serve metrics on different host or port.
@@ -36,12 +41,12 @@ var (
 	metricsPort         int32 = 8383
 	operatorMetricsPort int32 = 8686
 )
-var log = logf.Log.WithName("cmd")
 
 func printVersion() {
-	log.Info(fmt.Sprintf("Go Version: %s", runtime.Version()))
-	log.Info(fmt.Sprintf("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH))
-	log.Info(fmt.Sprintf("Version of operator-sdk: %v", sdkVersion.Version))
+	log.Infof("Go Version: %s", runtime.Version())
+	log.Infof("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH)
+	log.Infof("Version of operator-sdk: %v", sdkVersion.Version)
+	log.Infof("Kubeflow version: %v", Version)
 }
 
 func main() {
@@ -55,28 +60,18 @@ func main() {
 
 	pflag.Parse()
 
-	// Use a zap logr.Logger implementation. If none of the zap
-	// flags are configured (or if the zap flag set is not being
-	// used), this defaults to a production zap logger.
-	//
-	// The logger instantiated here can be changed to any logger
-	// implementing the logr.Logger interface. This logger will
-	// be propagated through the whole operator, generating
-	// uniform and structured logs.
-	logf.SetLogger(zap.Logger())
-
 	printVersion()
 
 	namespace, err := k8sutil.GetWatchNamespace()
 	if err != nil {
-		log.Error(err, "Failed to get watch namespace")
+		log.Errorf("Failed to get watch namespace. Error %v.", err)
 		os.Exit(1)
 	}
 
 	// Get a config to talk to the apiserver
 	cfg, err := config.GetConfig()
 	if err != nil {
-		log.Error(err, "")
+		log.Errorf("Error: %v.", err)
 		os.Exit(1)
 	}
 
@@ -84,7 +79,7 @@ func main() {
 	// Become the leader before proceeding
 	err = leader.Become(ctx, "kfctl-lock")
 	if err != nil {
-		log.Error(err, "")
+		log.Errorf("Error: %v.", err)
 		os.Exit(1)
 	}
 
@@ -96,7 +91,7 @@ func main() {
 		MetricsBindAddress: fmt.Sprintf("%s:%d", metricsHost, metricsPort),
 	})
 	if err != nil {
-		log.Error(err, "")
+		log.Errorf("Error: %v.", err)
 		os.Exit(1)
 	}
 
@@ -104,18 +99,18 @@ func main() {
 
 	// Setup Scheme for all resources
 	if err := apis.AddToScheme(mgr.GetScheme()); err != nil {
-		log.Error(err, "")
+		log.Errorf("Error: %v.", err)
 		os.Exit(1)
 	}
 
 	// Setup all Controllers
 	if err := controller.AddToManager(mgr); err != nil {
-		log.Error(err, "")
+		log.Errorf("Error: %v.", err)
 		os.Exit(1)
 	}
 
 	if err = serveCRMetrics(cfg); err != nil {
-		log.Info("Could not generate and serve custom resource metrics", "error", err.Error())
+		log.Errorf("Could not generate and serve custom resource metrics. Error: %v.", err.Error())
 	}
 
 	// Add to the below struct any other metrics ports you want to expose.
@@ -126,7 +121,7 @@ func main() {
 	// Create Service object to expose the metrics port(s).
 	service, err := metrics.CreateMetricsService(ctx, cfg, servicePorts)
 	if err != nil {
-		log.Info("Could not create metrics Service", "error", err.Error())
+		log.Errorf("Could not create metrics Service. Error: %v.", err.Error())
 	}
 
 	// CreateServiceMonitors will automatically create the prometheus-operator ServiceMonitor resources
@@ -134,19 +129,19 @@ func main() {
 	services := []*v1.Service{service}
 	_, err = metrics.CreateServiceMonitors(cfg, namespace, services)
 	if err != nil {
-		log.Info("Could not create ServiceMonitor object", "error", err.Error())
+		log.Errorf("Could not create ServiceMonitor object. Error: %v.", err.Error())
 		// If this operator is deployed to a cluster without the prometheus-operator running, it will return
 		// ErrServiceMonitorNotPresent, which can be used to safely skip ServiceMonitor creation.
 		if err == metrics.ErrServiceMonitorNotPresent {
-			log.Info("Install prometheus-operator in your cluster to create ServiceMonitor objects", "error", err.Error())
+			log.Errorf("Install prometheus-operator in your cluster to create ServiceMonitor objects. Error: %v.", err.Error())
 		}
 	}
 
-	log.Info("Starting the Cmd.")
+	log.Infof("Starting the Cmd.")
 
 	// Start the Cmd
 	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
-		log.Error(err, "Manager exited non-zero")
+		log.Errorf("Manager exited non-zero. Error: %v.", err)
 		os.Exit(1)
 	}
 }
