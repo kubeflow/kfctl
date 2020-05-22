d8ad369f608470c8a4152500084cae2caa8692b9
Haiqing Guo <289847235@qq.com>
2020-05-22 10:34:59 +0800

    feat: support httpproxy in KfConfig SyncCache

--- pkg/kfconfig/types.go
+++ pkg/kfconfig/types.go
@@ -516,7 +516,7 @@ func (c *KfConfig) SyncCache() error {
 				return errors.WithStack(err)
 			}
 		} else {
-			t := &http.Transport{}
+			t := &http.Transport{Proxy: http.ProxyFromEnvironment}
 			t.RegisterProtocol("file", http.NewFileTransport(http.Dir("/")))
 			t.RegisterProtocol("", http.NewFileTransport(http.Dir("/")))
 			hclient := &http.Client{Transport: t}
