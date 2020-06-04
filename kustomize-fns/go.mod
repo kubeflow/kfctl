74be6155104c285ff149a22eefc3e1ea69362269
Jeremy Lewi <jeremy+github@lewi.us>
2020-06-04 15:45:43 -0700

    kustomize fn to transform images (#345)
    * kustomize fn to transform images
    
    * This PR defines a kustomize/kpt function to transform images based on their prefix.
    
      * This is an alternative to using kfctl mirror overwrite
    
      * This works as follows.
    
        i) you define a function config like the following
    
           ```
            apiVersion: v1alpha1 # Define a transform to change all the image prefixes to use images from a different registry
         kind: ImagePrefix
         metadata:
           name: use-mirror-images-gcr
           annotations:
             config.kubernetes.io/function: |
               container:
                 image: gcr.io/kubeflow-images-public/kpt-fns:v1.0-rc.3-58-g616f986-dirty
         spec:
           imageMappings:
           - src: quay.io/jetstack
             dest: gcr.io/gcp-private-dev/jetstack # {"type":"string","x-kustomize":{"setBy":"kpt","partialSetters":[{"name":"gcloud.core.project","value":"gcp-private-dev"}]}}
           - src: gcr.io/kubeflow-images-public
             dest: gcr.io/gcp-private-dev # {"type":"string","x-kustomize":{"setBy":"kpt","partialSetters":[{"name":"gcloud.core.project","value":"gcp-private-dev"}]}}
           ```
    
       ii) You use kpt to appy the function e.g.
    
           ```
           kpt fn ${DIR}
           ```
    
           * Typically you would apply this to the hydrated manfiests
           * The transform will look for any objects with `image-prefix.kubeflow.org` annotation
             and then map the image prefixes specified in that annotation
    
    * The kpt function has a number of advantages over the kfctl functionality
    
      * In particular the transform configuration (i.e. the annotation) doesn't need to change
        when a user upgrades and the image tags change. Since the transform is applied after
        hydration we would end up applying the transform to any docker images
    
      * `kpt fns` are more scalable then baking all the functionality into a single go binary
    
        * It also looks like this functionality will be upstreamed into kustomize
    
    * kubeflow/manifests#1033 proposed a similar transformer for images.
    
      * This is different only in we are using kpt/kustomize functions rather than kustomize transformations
      * kpt functions are more flexible because its YAML in YAML out; so you could just as easily
    
    * Trigger kustomize-fns unittests.
    
    * Revert go.mod changes.
    
    * Address comments.

---
+++ kustomize-fns/go.mod
@@ -0,0 +1,10 @@
+module github.com/kubeflow/kfctl/kustomize-fns
+
+go 1.13
+
+require (
+	github.com/pkg/errors v0.8.0
+	github.com/spf13/cobra v1.0.0
+	k8s.io/apimachinery v0.18.3
+	sigs.k8s.io/kustomize/kyaml v0.1.13
+)
--- kustomize-fns/remove-namespace/main.go
+++ kustomize-fns/remove-namespace/main.go
@@ -3,46 +3,49 @@
 
 // Package main implements an injection function for resource reservations and
 // is run with `kustomize config run -- DIR/`.
+
+// TODO(https://github.com/kubeflow/gcp-blueprints/issues/27): We should make this configurable and follow
+// the model of image-prefix. This means instead of using a separate main.go we should use kustomize-fns/main.go
+// and just register this function in the dispatcher.
 package main
 
 import (
-
-"fmt"
-"os"
-"sigs.k8s.io/kustomize/kyaml/fn/framework"
-"sigs.k8s.io/kustomize/kyaml/yaml"
-"strings"
+	"fmt"
+	"os"
+	"sigs.k8s.io/kustomize/kyaml/fn/framework"
+	"sigs.k8s.io/kustomize/kyaml/yaml"
+	"strings"
 )
 
 type groupKind struct {
 	Group string
-	Kind string
+	Kind  string
 }
 
 var (
 
 	// List of clusterResources to match and remove namespace from.
-	// TODO(jlewi): How could we make this configurable?
+	// TODO(jlewi): How could we make this configurable? I think you want to put the relevant data
+	// into the YAML files that get processed by the transform. We could define an annotation and then
+	// the transform could look for any resource with this annotation and remove the namespace if it exists.
 	clusterKinds = []groupKind{
 		{
-			Kind: "Profile",
+			Kind:  "Profile",
 			Group: "kubeflow.org",
-
 		},
 		{
-			Kind: "ClusterRbacConfig",
+			Kind:  "ClusterRbacConfig",
 			Group: "rbac.istio.io",
 		},
 		{
-			Kind: "ClusterIssuer",
+			Kind:  "ClusterIssuer",
 			Group: "cert-manager.io",
 		},
 		{
-			Kind: "CompositeController",
+			Kind:  "CompositeController",
 			Group: "metacontroller.k8s.io",
 		},
 	}
-
 )
 
 func main() {
@@ -70,7 +73,7 @@ func getGroup(apiVersion string) string {
 	}
 
 	// Strip the last piece as the version
-	pieces = pieces[0:len(pieces) -1 ]
+	pieces = pieces[0 : len(pieces)-1]
 	return strings.Join(pieces, "/")
 }
 
@@ -99,12 +102,10 @@ func removeNamespace(r *yaml.RNode) error {
 		return nil
 	}
 
-
 	return r.PipeE(
 		yaml.LookupCreate(yaml.ScalarNode, "metadata"),
-		// set the field value to the cpuSize
 		yaml.FieldClearer{
 			Name: "namespace",
 		},
-		)
+	)
 }
