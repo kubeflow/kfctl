9a3621e2b2ce6d2baa5c78acccec5028c4e4cbe1
Yao Xiao <XYSHOW@amazon.com>
2020-07-24 13:18:16 -0700

    Cherry pick #378 #379 #380 to v1.1-branch (#381)
    * Adapt to v3 manifest for aws (#378)
    
    * Improve AWS kfctl (#379)
    
    * Use istio-parameter in manifests (#380)

--- pkg/kfapp/aws/aws.go
+++ pkg/kfapp/aws/aws.go
@@ -450,11 +450,7 @@ func (aws *Aws) Generate(resources kftypes.ResourceEnum) error {
 		return errors.WithStack(err)
 	}
 
-	if err := aws.kfDef.SetApplicationParameter("aws-alb-ingress-controller", "clusterName", aws.kfDef.Name); err != nil {
-		return errors.WithStack(err)
-	}
-
-	if err := aws.kfDef.SetApplicationParameter("istio-ingress", "namespace", IstioNamespace); err != nil {
+	if err := aws.kfDef.SetApplicationParameter("aws-alb-ingress-controller", "cluster-name", aws.kfDef.Name); err != nil {
 		return errors.WithStack(err)
 	}
 
@@ -471,10 +467,6 @@ func (aws *Aws) Generate(resources kftypes.ResourceEnum) error {
 		}
 	} else {
 		if pluginSpec.Auth.Cognito != nil {
-			if err := aws.kfDef.SetApplicationParameter("istio", "clusterRbacConfig", "ON"); err != nil {
-				return errors.WithStack(err)
-			}
-
 			if err := aws.kfDef.SetApplicationParameter("istio-ingress", "CognitoUserPoolArn", pluginSpec.Auth.Cognito.CognitoUserPoolArn); err != nil {
 				return errors.WithStack(err)
 			}
