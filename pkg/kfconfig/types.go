cc8fea57c05cdc601aaceed7d8dd35fd3d9df343
Jiaxin Shan <seedjeffwan@gmail.com>
2020-11-15 15:23:04 -0800

    Automated cherry pick of #438: fail build on missing application name (#446)
    * fail build on missing application name
    
    * add application index on missing application error
    
    * update index syntax

--- pkg/kfconfig/types.go
+++ pkg/kfconfig/types.go
@@ -845,7 +845,7 @@ func (c *KfConfig) SetApplicationParameter(appName string, paramName string, val
 			"profiles":         KfAppsStackName,
 			"dex":              "dex",
 			// Spartakus is its own application because we want kfctl to be able to remove it.
-			"spartakus":                  "spartakus",
+			"spartakus": "spartakus",
 			// AWS Specific
 			"aws-alb-ingress-controller": KfAppsStackName,
 			"istio-ingress":              "istio-ingress",
