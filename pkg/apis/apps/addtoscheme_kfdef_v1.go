c9d89f178d03b083e9124e072555f687aed0e113
Vaishnavi Hire <vhire@redhat.com>
2021-04-15 07:06:36 -0400

    Implement clean uninstall of operator (#117)
    * Implement clean uninstall of operator
    
    This commit includes changes in the controller to
    remove resources created in the namespaces other than
    the operator namespace. Removal of external resources
    is triggered once a delete configMap is added to the
    operator namespace
    
    * Update go mod
    
    Commands Run: go mod tidy

--- pkg/apis/apps/addtoscheme_kfdef_v1.go
+++ pkg/apis/apps/addtoscheme_kfdef_v1.go
@@ -2,9 +2,12 @@ package apps
 
 import (
 	v1 "github.com/kubeflow/kfctl/v3/pkg/apis/apps/kfdef/v1"
+	operatorsv1alpha1 "github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1alpha1"
 )
 
 func init() {
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
-	AddToSchemes = append(AddToSchemes, v1.SchemeBuilder.AddToScheme)
+	AddToSchemes = append(AddToSchemes,
+		v1.SchemeBuilder.AddToScheme,
+		operatorsv1alpha1.AddToScheme)
 }
